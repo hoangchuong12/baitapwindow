ad6228134e9703d2f4fb357b7790f50fba143daa
hoangchuong12 <chuong12072003@gamil.com>
2023-09-23 18:27:06 +0700

    dsfd

---
+++ buoi1bai03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi1bai03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("nhap vao so nguyen duong n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("vui long nhap lai so nguyen duong");
+            }
+            else
+            {
+                int sum = 0;
+
+                for (int i = 1; i <= 2 * n - 1; i += 2)
+                {
+                    sum += i;
+                }
+
+                Console.WriteLine($"tong cac so le {2 * n - 1} là: {sum}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ buoi1bai05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi1bai05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap sp luong sin vien: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] dssinhvien = NhapSV(n);
+            Console.WriteLine("Danh sach sinh vien:");
+            XuatSV(dssinhvien, n);
+            Console.ReadKey();
+        }
+        static string[] NhapSV(int n)
+        {
+            string[] dssinhvien = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"nhap ho ten sinh vien {i + 1}: ");
+                dssinhvien[i] = Console.ReadLine();
+            }
+
+            return dssinhvien;
+        }
+
+        static void XuatSV(string[] dssinhvien, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"sinh vien {i + 1}: {dssinhvien[i]}");
+           
+            }
+        }
+    }
+}
